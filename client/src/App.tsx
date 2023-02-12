945913ae5649a1f926f555f20ea9ec50cbada414
Amrit Bhusal <45762946+AmritBhusal@users.noreply.github.com>
2023-02-12 14:35:52 +0545

    connected wallet and and fetching data from chain

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,38 @@
-import { Layout, Row, Col } from "antd";
+import { Layout, Row, Col, Button  } from "antd";
+import { WalletSelector } from "@aptos-labs/wallet-adapter-ant-design";
+import "@aptos-labs/wallet-adapter-ant-design/dist/index.css";
+import { AptosClient } from "aptos";
+import { useWallet } from "@aptos-labs/wallet-adapter-react";
+import { useState, useEffect } from "react";
+
+
+const NODE_URL = "https://fullnode.devnet.aptoslabs.com";
+const client = new AptosClient(NODE_URL);
 
 function App() {
+  const { account } = useWallet();
+
+  const [accountHasList, setAccountHasList] = useState<boolean>(false);
+
+  useEffect(() => {
+    fetchList();
+  }, [account?.address]);
+
+  const fetchList = async () => {
+    if (!account) return [];
+    // change this to be your module account address
+    const moduleAddress = "0xa1e3aa355555eb0b94cb3de6c98c1a5dd33c45a3647b3b5697c21d4719339b2e";
+    try {
+      const TodoListResource = await client.getAccountResource(
+        account.address,
+        `${moduleAddress}::todolist::TodoList`
+      );
+      setAccountHasList(true);
+    } catch (e: any) {
+      setAccountHasList(false);
+    }
+  };
+
   return (
     <>
       <Layout>
@@ -9,10 +41,19 @@ function App() {
             <h1>Our todolist</h1>
           </Col>
           <Col span={12} style={{ textAlign: "right", paddingRight: "200px" }}>
-            <h1>Connect Wallet</h1>
+            <WalletSelector />
           </Col>
         </Row>
       </Layout>
+      {!accountHasList && (
+        <Row gutter={[0, 32]} style={{ marginTop: "2rem" }}>
+          <Col span={8} offset={8}>
+            <Button block type="primary" style={{ height: "40px", backgroundColor: "#3f67ff" }}>
+              Add new list
+            </Button>
+          </Col>
+        </Row>
+      )}
     </>
   );
 }
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { PetraWallet } from "petra-plugin-wallet-adapter";
+import { AptosWalletAdapterProvider } from "@aptos-labs/wallet-adapter-react";
 import reportWebVitals from './reportWebVitals';
+
+const wallets = [new PetraWallet()];
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <AptosWalletAdapterProvider plugins={wallets} autoConnect={true}>
+  <App />
+</AptosWalletAdapterProvider>
   </React.StrictMode>
 );
 
