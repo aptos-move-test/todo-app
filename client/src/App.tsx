c80114de4132e71e201d50acf6e477e80e2c8488
Amrit Bhusal <45762946+AmritBhusal@users.noreply.github.com>
2023-02-12 16:47:48 +0545

    Submitting data to chain and handelling the tasks

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
-import { Layout, Row, Col, Button  } from "antd";
+import { Layout, Row, Col, Button, Spin, List, Checkbox, Input } from "antd";
+import { CheckboxChangeEvent } from 'antd/es/checkbox';
 import { WalletSelector } from "@aptos-labs/wallet-adapter-ant-design";
 import "@aptos-labs/wallet-adapter-ant-design/dist/index.css";
 import { AptosClient } from "aptos";
@@ -6,13 +7,35 @@ import { useWallet } from "@aptos-labs/wallet-adapter-react";
 import { useState, useEffect } from "react";
 
 
-const NODE_URL = "https://fullnode.devnet.aptoslabs.com";
-const client = new AptosClient(NODE_URL);
+
+export const NODE_URL = "https://fullnode.devnet.aptoslabs.com";
+export const client = new AptosClient(NODE_URL);
+
+export const moduleAddress = "0xa1e3aa355555eb0b94cb3de6c98c1a5dd33c45a3647b3b5697c21d4719339b2e";
+
+type Task = {
+  address: string;
+  completed: boolean;
+  content: string;
+  task_id: string;
+};
 
 function App() {
-  const { account } = useWallet();
+  const { account, signAndSubmitTransaction } = useWallet();
 
   const [accountHasList, setAccountHasList] = useState<boolean>(false);
+
+  const [transactionInProgress, setTransactionInProgress] = useState<boolean>(false);
+
+  const [newTask, setNewTask] = useState<string>("");
+
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+
+  const onWriteTask = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setNewTask(value);
+  };
 
   useEffect(() => {
     fetchList();
@@ -20,41 +43,243 @@ function App() {
 
   const fetchList = async () => {
     if (!account) return [];
-    // change this to be your module account address
-    const moduleAddress = "0xa1e3aa355555eb0b94cb3de6c98c1a5dd33c45a3647b3b5697c21d4719339b2e";
     try {
       const TodoListResource = await client.getAccountResource(
         account.address,
         `${moduleAddress}::todolist::TodoList`
       );
       setAccountHasList(true);
+      // tasks table handle
+      const tableHandle = (TodoListResource as any).data.tasks.handle;
+      // tasks table counter
+      const taskCounter = (TodoListResource as any).data.task_counter;
+
+      let tasks = [];
+      let counter = 1;
+      while (counter <= taskCounter) {
+        const tableItem = {
+          key_type: "u64",
+          value_type: `${moduleAddress}::todolist::Task`,
+          key: `${taskCounter}`,
+        };
+        const task = await client.getTableItem(tableHandle, tableItem);
+        tasks.push(task);
+        counter++;
+      }
+          // set tasks in local state
+      setTasks(tasks);
     } catch (e: any) {
       setAccountHasList(false);
     }
   };
 
+  const onTaskAdded = async () => {
+    // check for connected account
+    if (!account) return;
+    setTransactionInProgress(true);
+    // build a transaction payload to be submited
+    const payload = {
+      type: "entry_function_payload",
+      function: `${moduleAddress}::todolist::create_task`,
+      type_arguments: [],
+      arguments: [newTask],
+    };
+
+    try {
+      // sign and submit transaction to chain
+      const response = await signAndSubmitTransaction(payload);
+      // wait for transaction
+      await client.waitForTransaction(response.hash);
+
+            // hold the latest task.task_id from our local state
+      const latestId = tasks.length > 0 ? parseInt(tasks[tasks.length - 1].task_id) + 1 : 1;
+
+      // build a newTaskToPush objct into our local state
+      const newTaskToPush = {
+        address: account.address,
+        completed: false,
+        content: newTask,
+        task_id: latestId + "",
+      };
+
+      // Create a new array based on current state:
+      let newTasks = [...tasks];
+
+      // Add item to it
+      newTasks.unshift(newTaskToPush);
+
+      // Set state
+      setTasks(newTasks);
+            // clear input text
+      setNewTask("");
+    } catch (error: any) {
+      console.log("error", error);
+    } finally {
+      setTransactionInProgress(false);
+    }
+  };
+
+  const addNewList = async () => {
+    if (!account) return [];
+    setTransactionInProgress(true);
+    // build a transaction payload to be submited
+    const payload = {
+      type: "entry_function_payload",
+      function: `${moduleAddress}::todolist::create_list`,
+      type_arguments: [],
+      arguments: [],
+    };
+    try {
+      // sign and submit transaction to chain
+      const response = await signAndSubmitTransaction(payload);
+      // wait for transaction
+      await client.waitForTransaction(response.hash);
+      setAccountHasList(true);
+    } catch (error: any) {
+      setAccountHasList(false);
+    } finally {
+      setTransactionInProgress(false);
+    }
+  };
+
+  const onCheckboxChange = async (
+    event: CheckboxChangeEvent,
+    taskId: string
+  ) => {
+    if (!account) return;
+    if (!event.target.checked) return;
+    setTransactionInProgress(true);
+    const payload = {
+      type: "entry_function_payload",
+      function:
+        `${moduleAddress}::todolist::complete_task`,
+      type_arguments: [],
+      arguments: [taskId],
+    };
+
+    try {
+      // sign and submit transaction to chain
+      const response = await signAndSubmitTransaction(payload);
+      // wait for transaction
+      await client.waitForTransaction(response.hash);
+
+      setTasks((prevState) => {
+        const newState = prevState.map((obj) => {
+          // if task_id equals the checked taskId, update completed property
+          if (obj.task_id === taskId) {
+            return { ...obj, completed: true };
+          }
+
+          // otherwise return object as is
+          return obj;
+        });
+
+        return newState;
+      });
+    } catch (error: any) {
+      console.log("error", error);
+    } finally {
+      setTransactionInProgress(false);
+    }
+  };
+
   return (
     <>
-      <Layout>
-        <Row align="middle">
-          <Col span={10} offset={2}>
-            <h1>Our todolist</h1>
-          </Col>
-          <Col span={12} style={{ textAlign: "right", paddingRight: "200px" }}>
-            <WalletSelector />
-          </Col>
-        </Row>
-      </Layout>
-      {!accountHasList && (
-        <Row gutter={[0, 32]} style={{ marginTop: "2rem" }}>
-          <Col span={8} offset={8}>
-            <Button block type="primary" style={{ height: "40px", backgroundColor: "#3f67ff" }}>
-              Add new list
-            </Button>
-          </Col>
-        </Row>
+    <Layout>
+      <Row align="middle">
+        <Col span={10} offset={2}>
+          <h1>Todolist</h1>
+        </Col>
+        <Col span={12} style={{ textAlign: "right", paddingRight: "200px" }}>
+          <WalletSelector />
+        </Col>
+      </Row>
+    </Layout>
+    {!accountHasList && (
+      <Row gutter={[0, 32]} style={{ marginTop: "2rem" }}>
+        <Col span={8} offset={8}>
+        <>
+    <Spin spinning={transactionInProgress}>
+    {
+  !accountHasList ? (
+    <Row gutter={[0, 32]} style={{ marginTop: "2rem" }}>
+      <Col span={8} offset={8}>
+        <Button
+          disabled={!account}
+          block
+          onClick={addNewList}
+          type="primary"
+          style={{ height: "40px", backgroundColor: "#3f67ff" }}
+        >
+          Add new list
+        </Button>
+      </Col>
+    </Row>
+  ) : (
+    <Row gutter={[0, 32]} style={{ marginTop: "2rem" }}>
+      <Col span={8} offset={8}>
+      <Input.Group compact>
+      <Input
+        onChange={(event) => onWriteTask(event)} // add this
+        style={{ width: "calc(100% - 60px)" }}
+        placeholder="Add a Task"
+        size="large"
+        value={newTask} // add this
+      />
+       <Button
+        onClick={onTaskAdded} // add this
+        type="primary"
+        style={{ height: "40px", backgroundColor: "#3f67ff" }}
+      >
+        Add
+      </Button>
+      </Input.Group>
+    </Col>
+      <Col span={8} offset={8}>
+        {tasks && (
+          <List
+            size="small"
+            bordered
+            dataSource={tasks}
+            renderItem={(task: any) => (
+<List.Item
+  actions={[
+    <div>
+      {task.completed ? (
+        <Checkbox defaultChecked={true} disabled />
+      ) : (
+        <Checkbox
+          onChange={(event) =>
+            onCheckboxChange(event, task.task_id)
+          }
+        />
       )}
-    </>
+    </div>,
+  ]}
+>                <List.Item.Meta
+                  title={task.content}
+                  description={
+                    <a
+                      href={`https://explorer.aptoslabs.com/account/${task.address}/`}
+                      target="_blank"
+                    >{`${task.address.slice(0, 6)}...${task.address.slice(-5)}`}</a>
+                  }
+                />
+              </List.Item>
+            )}
+          />
+        )}
+      </Col>
+    </Row>
+  )
+}
+      </Spin>
+  </>
+        </Col>
+      </Row>
+    )}
+  
+  </>
   );
 }
 export default App;
